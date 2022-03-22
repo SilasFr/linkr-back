ac6ffa43756398533070b8be4187cacb61525a48
Silas <silas23.fr@gmail.com>
2022-03-21 22:50:02 -0300

    feat: config project dependecies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import express, { json } from "express";
 import cors from "cors";
+import dotenv, { config } from "dotenv";
+app.use(config);
+
 const app = express();
 app.use(cors());
 app.use(json());
 
-app.listen(5000, () => console.log("running on PORT 5000"));
+app.listen(process.env.PORT, () => console.log("running on PORT 5000"));
