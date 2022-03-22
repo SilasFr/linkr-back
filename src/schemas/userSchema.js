2b18650c38a31a36207ab7885a64a79a44329b01
Silas <silas23.fr@gmail.com>
2022-03-22 09:35:02 -0300

    docs: add repository pattern; feat: insert user into database

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -4,7 +4,7 @@ const userSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
   userName: Joi.string().required(),
-  pictureUrl: Joi.uri().required(),
+  pictureUrl: Joi.string().uri().required(),
 });
 
 export default userSchema;
