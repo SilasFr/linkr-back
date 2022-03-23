b40e0381e4abb79e075bdaf048a5c52765adaf55
Tarcísio F. S. Teles <tfst@poli.br / tfst@ecomp.poli.br>
2022-03-23 13:16:44 -0300

    test: inserting newPost (pending token validation), lunchbreak

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -5,9 +5,12 @@ import connection from '../database.js';
 
 export async function newPost(req, res) {
   const { link, description } = res.locals.newPostData;
-  console.log('reached Controller: ', res.locals);
   try {
-    return res.sendStatus(501);
+    await connection.query(`
+      INSERT INTO posts (author, link, description)
+      VALUES (1, $1, $2);
+      `, [link, description]);
+    return res.sendStatus(201);
   } catch (error) {
     return res.status(500).send('!erro! cadastrando novo post');
   }
