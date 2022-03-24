10729023dbd7137161fc4ef5c327f2e4e09ba703
Silas <silas23.fr@gmail.com>
2022-03-23 21:00:58 -0300

    feat: get posts from database

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -20,15 +20,17 @@ export async function getPosts(req, res) {
     },
   ];
   try {
-    // const { rows } = await postsRepository.getPosts();
-    // const posts = rows.splice(0, 20);
-    // res.send(posts).status(200);
-    let time;
+    const { rows } = await postsRepository.getPosts();
+
     let result = [];
     test.map((element) => {
+      // TROCAR PARA rows.map
+      let item = {
+        ...element,
+      };
       urlMetadata(element.link)
         .then(async function ({ url, title, description, image }) {
-          let item = { url, title, description, image };
+          item = { ...item, url, title, description, image };
           result.push(item);
         })
         .catch((error) => console.log("error: ", error));
@@ -41,22 +43,3 @@ export async function getPosts(req, res) {
     res.status(500).send(e);
   }
 }
-
-// export async function getPosts(req, res) {
-//   const userToken = req.headers.authorization.replaceAll('Bearer ', '');
-//   try {
-//     const { rows: existentToken } = await connection.query(`
-//       SELECT * FROM sessions WHERE token=$1
-//     `, [userToken]);
-//     if (existentToken.length === 0) {
-//       return res.status(404).send('!erro! token invalido ou expirado');
-//     }
-
-//     const allPosts = await connection.query(`
-//       SELECT * FROM posts;
-//     `);
-//     return res.status(200).send(allPosts.rows);
-//   } catch (error) {
-//     return res.status(500).send('!erro! obtendo posts');
-//   }
-// }
--- src/repositories/postsRepository.js
+++ src/repositories/postsRepository.js
@@ -2,7 +2,13 @@ import connection from "../database.js";
 
 async function getPosts() {
   return connection.query(`
-        SELECT * FROM posts ORDER BY createdAt DESC
+        SELECT p.link, p.description, u.name AS "userName", u."profilePic" FROM "postsTopics" 
+          JOIN posts p ON "postsTopics"."postId" = p.id
+          JOIN topics ON topics.id = "postsTopics"."topicId"
+          JOIN users u ON p.author = u.id
+        GROUP BY "postsTopics"."postId"
+        ORDER BY createdAt DESC
+        LIMIT 20
     `);
 }
 
