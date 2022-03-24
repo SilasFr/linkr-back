72876270a245479acfbf53a79a3cd2242620fee6
fernandinho <fernando_yudi_yaeda@hotmail.com>
2022-03-24 20:28:40 -0300

    fix: database script

--- database/scripts/001-cria-banco.sql
+++ database/scripts/001-cria-banco.sql
@@ -1,55 +1,53 @@
 CREATE TABLE users(
-	id SERIAL PRIMARY KEY,
-	name TEXT NOT NULL,
-	email TEXT UNIQUE NOT NULL,
-	password VARCHAR(255) NOT NULL,
-	"profilePic" TEXT NOT NULL
-);
-
-CREATE TABLE sessions(
-	id SERIAL PRIMARY KEY,
-	"userId" INTEGER NOT NULL REFERENCES users(id),
-	token TEXT NOT NULL,
-	"expiresAt" TIMESTAMP WITH TIME ZONE NOT NULL
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    "profilePic" TEXT NOT NULL
 );
 
 CREATE TABLE links(
-	id SERIAL PRIMARY KEY,
-	link TEXT NOT NULL,
-	title TEXT NOT NULL,
-	description TEXT,
-	image TEXT NOT NULL,	
-)
-
-CREATE TABLE linksPosts(
-	id SERIAL PRIMARY KEY,
-	"linkId" INTEGER NOT NULL REFERENCES link(id),
-	"postsId" INTEGER NOT NULL REFERENCES post(id)
-)
+    id SERIAL PRIMARY KEY,
+    link TEXT NOT NULL,
+    title TEXT NOT NULL,
+    description TEXT,
+    image TEXT NOT NULL
+);
 
 CREATE TABLE posts(
-	id SERIAL PRIMARY KEY,
-	author INTEGER NOT NULL REFERENCES users(id),
-	description TEXT NOT NULL,
-	"linkId" INTEGER NOT NULL REFERENCES "linkPosts"("linkId"),
-	createdAt TIMESTAMP NOT NULL DEFAULT NOW()
+    id SERIAL PRIMARY KEY,
+    author INTEGER NOT NULL REFERENCES users(id),
+    description TEXT NOT NULL,
+    "linkId" INTEGER NOT NULL REFERENCES links(id),
+    "createdAt" TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE sessions(
+    id SERIAL PRIMARY KEY,
+    "userId" INTEGER NOT NULL REFERENCES users(id),
+    token TEXT NOT NULL,
+    "expiresAt" TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE TABLE "linksPosts"(
+    id SERIAL PRIMARY KEY,
+    "linkId" INTEGER NOT NULL REFERENCES links(id),
+    "postsId" INTEGER NOT NULL REFERENCES posts(id)
 );
 
 CREATE TABLE "likedPost"(
-	id SERIAL PRIMARY KEY,
-	"postId" INTEGER NOT NULL REFERENCES posts(id),
-	"likeAuthor" INTEGER NOT NULL REFERENCES users(id)
+    id SERIAL PRIMARY KEY,
+    "postId" INTEGER NOT NULL REFERENCES posts(id),
+    "likeAuthor" INTEGER NOT NULL REFERENCES users(id)
 );
 
 CREATE TABLE topics(
-	id SERIAL PRIMARY KEY,
-	topic TEXT NOT NULL UNIQUE
+    id SERIAL PRIMARY KEY,
+    topic TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE "postsTopics"(
-	id SERIAL PRIMARY KEY,
-	"postId" INTEGER NOT NULL REFERENCES posts(id),
-	"topicId" INTEGER NOT NULL REFERENCES topics(id)
+    id SERIAL PRIMARY KEY,
+    "postId" INTEGER NOT NULL REFERENCES posts(id),
+    "topicId" INTEGER NOT NULL REFERENCES topics(id)
 );
-
-
