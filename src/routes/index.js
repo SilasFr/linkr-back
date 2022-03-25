89992cdc35111bf753431f242e0984eb88fdbff0
Tarcísio F. S. Teles <tfst@poli.br / tfst@ecomp.poli.br>
2022-03-24 22:28:13 -0300

    fix: fixed new-post insertion using links/posts tables

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ import userRouter from "./userRouter.js";
 import loginRouter from "./loginRouter.js";
 import postsRouter from "./postsRouter.js";
 import logoutRouter from "./logoutRouter.js";
-import postsRouter from './postsRouter.js';
 
 const router = Router();
 
