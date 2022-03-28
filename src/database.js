40f4f01a1d279d1ebe04151cfa8f819e919c1da5
Matheus <morais.matheus.web@gmail.com>
2022-03-28 20:08:53 -0300

    fix: conflicts

--- src/database.js
+++ src/database.js
@@ -6,19 +6,11 @@ dotenv.config();
 const { Pool } = pg;
 
 const databaseConfig = {
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: "123456",
-  database: "linkr",
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
-
-// const databaseConfig = {
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// };
 
 const connection = new Pool(databaseConfig);
 
